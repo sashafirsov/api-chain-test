967dbc390f30c7b7a9a41bf7c82c8f512d7a6210
suns <suns@firsov.net>
2021-10-26 00:28:24 -0700

    dist/ removed from gitignore

---
+++ dist/CssChainElement.js
@@ -0,0 +1,35 @@
+import $ from '../../css-chain@1.0.4/CssChain.js';
+
+    export class
+CssChainElement extends HTMLElement
+{
+    constructor()
+    {
+        super();
+        this.title = 'Hey there';
+        this.counter = 0;
+
+        const t = document.createElement('div');
+        t.innerHTML = this.template;
+        // this.innerHTML = this.template;
+
+        this.attachShadow({mode: 'open'}).appendChild(t);
+        this.$('button').addEventListener('click', ()=>this.__increment() );
+    }
+
+    get template()
+    {   return `
+<slot></slot> &bull;
+<b></b><br/>
+<input /><button>🛒</button>
+`
+    }
+
+    $( css, protoArr ){ return $( css, this.shadowRoot, protoArr ); }
+
+    __increment()
+    {   const slotContent = this.$('slot')[0].assignedNodes()[0].textContent;
+        this.$('b').innerHTML += `<span>${ slotContent }</span>`;
+        this.counter = this.$('input').value = this.$('span').length;
+    }
+}
---
+++ dist/css-chain-element.js
@@ -0,0 +1,5 @@
+import { CssChainElement } from './CssChainElement.js';
+
+window.customElements.define( 'css-chain', CssChainElement);
+
+export default CssChainElement;
