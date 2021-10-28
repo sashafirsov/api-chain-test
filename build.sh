2470f6437556ffc685be835d1819c21e0d94de66
suns <suns@firsov.net>
2021-10-27 21:49:22 -0700

    demo component fix

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo $PACKAGE_VERSION
 
 rm -rf dist
 mkdir dist
-cp src/*.html dist
+sed "s/..\/src\/css-chain-element.js/css-chain-element.js/" src/demo.html >dist/demo.html
 
 # https://kangax.github.io/compat-table/es2016plus/
 esbuild src/*.js --minify --sourcemap --target=chrome97,firefox95,safari15,edge96 --outdir=dist
