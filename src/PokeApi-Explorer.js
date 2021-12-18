977ed04bb96345d0be7d28d39c0816c7680bcc56
Sasha Firsov <suns@firsov.net>
2021-12-18 10:45:33 -0800

    Update PokeApi-Explorer.js

--- src/PokeApi-Explorer.js
+++ src/PokeApi-Explorer.js
@@ -1,5 +1,5 @@
 import FetchElement from 'https://unpkg.com/slotted-element@1.0.3/fetch-element.js';
-import { CssChain as $$ } from "./CssChain.js";
+import { CssChain as $$ } from "https://unpkg.com/css-chain@1.0.7/CssChain.js";
 const $ = $$()
 , arr2str = (arr,cb, separator='') => arr.map(cb).join(separator)
 ,   isImg = url => url && url.endsWith && ['png','gif','svg'].find( x=>url.endsWith(x) );
