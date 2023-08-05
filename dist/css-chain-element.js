0fe1ae4c55c3e8a1f0440ce0d138419c36c1113e
Sasha Firsov <suns@firsov.net>
2023-08-05 12:12:15 -0700

    1.1.8

--- dist/css-chain-element.js
+++ dist/css-chain-element.js
@@ -1,2 +1,2 @@
-import{CssChainElement as e}from"./CssChainElement.js";window.customElements.define("css-chain",e);export default e;
+"use strict";import{CssChainElement as e}from"./CssChainElement.js";window.customElements.define("css-chain",e);export default e;
 //# sourceMappingURL=css-chain-element.js.map
